749de7a35b910cab07a8d65daaeeb332897ea229
lingxueli <ll2871@columbia.edu>
2020-02-02 23:12:37 -0700

    mock create user request and test response

---
+++ src/test/java/com/example/demo/controllers/UserControllerTest.java
@@ -0,0 +1,53 @@
+package com.example.demo.controllers;
+
+import com.example.demo.TestUtils;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.CartRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.requests.CreateUserRequest;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class UserControllerTest {
+    private UserController userController;
+
+    private UserRepository userRepo = mock(UserRepository.class);
+
+    private CartRepository cartRepo = mock(CartRepository.class);
+
+    private BCryptPasswordEncoder encoder = mock(BCryptPasswordEncoder.class);
+    @Before
+    public void setUp(){
+        userController = new UserController();
+        TestUtils.injectObject(userController, "userRepository", userRepo);
+        TestUtils.injectObject(userController, "cartRepository", cartRepo);
+        TestUtils.injectObject(userController, "bCryptPasswordEncoder", encoder);
+    }
+
+    @Test
+    public void create_user_happy_path() throws Exception {
+        // stubbing test
+        when(encoder.encode("testPassword")).thenReturn("thisIsHashed");
+
+        CreateUserRequest r = new CreateUserRequest();
+        r.setUsername("test");
+        r.setPassword("testPassword");
+        r.setConfirmPassword("testPassword");
+
+        final ResponseEntity<User> response = userController.createUser(r);
+        assertNotNull(response);
+        assertEquals(200 , response.getStatusCodeValue());
+
+        User u = response.getBody();
+        assertNotNull(u);
+        assertEquals(0, u.getId());
+        assertEquals("test", u.getUsername());
+        assertEquals("thisIsHashed", u.getPassword());
+    }
+}
